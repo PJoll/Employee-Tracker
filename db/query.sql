93c462441db6fe205be535bbb217dcf476b8850a
Peter Jolley <peterjolley456@gmail.com>
2022-05-31 21:40:18 +0930

    .env file created query commented out at this satge

--- db/query.sql
+++ db/query.sql
@@ -1,5 +1,5 @@
-SELECT department.department_name AS departmewnt, roles.salary
-FROM roles
-LEFT JOIN department
-ON roles.department_id = department.id
-ORDER BY department.department_name
+-- SELECT department.department_name AS departmewnt, roles.salary
+-- FROM roles
+-- LEFT JOIN department
+-- ON roles.department_id = department.id
+-- ORDER BY department.department_name
